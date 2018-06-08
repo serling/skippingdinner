1c9db1feade70e385c51a82ef02a26e1ec403b9a
Thomas Christoffersen <t.christoffersen@creuna.no>
2018-06-08 17:34:44 +0200

    new page, travis fix

---
+++ source/pages/people.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Page from '../components/page';
+import Heading from '../components/heading';
+import Row from '../components/row';
+
+const People = () => (
+  <Page>
+    <Row>
+      <Row.Content>
+        <Heading>People</Heading>
+      </Row.Content>
+    </Row>
+  </Page>
+);
+
+export default People;
--- source/routes.jsx
+++ source/routes.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Home from './pages/home';
+import People from './pages/people';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/people" component={People} />
   </Switch>
 );
 
